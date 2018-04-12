4049bb2d61441794bcbb9d5c649e6e64ef3ed32e
Martin Weltler <weltlerm@gmail.com>
2018-04-12 17:46:40 +0200

    feat(todo)add assignee class

---
+++ src/main/java/com/greenfoxacademy/todolistwithmsql/repository/AssigneeRepo.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.todolistwithmsql.repository;
+
+import com.greenfoxacademy.todolistwithmsql.models.Assignee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AssigneeRepo extends CrudRepository<Assignee, Long> {
+  
+}
